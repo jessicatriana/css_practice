68e869b8c770f5a4662339d3bcd123062021196f
jessicatriana <jessica.triana.tx@gmail.com>
2020-06-08 09:32:12 -0500

    Boolean Values

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ console.log(parseFloat(example))
 // Prints 7.7700 (the amount of decimal places you set)
 console.log(example.toFixed(4))
 
-// EXAMPLES OF NUMEBER METHODS
+// EXAMPLES OF NUMBER METHODS
 
 let example1 = parseInt('Hello 33 World 22')
 // Prints NaN
@@ -55,5 +55,39 @@ console.log(example3)
 let example4 = (200.0).toFixed(2)
 // Prints "200.00"
 console.log(example4)
+
+// BOOLEAN VALUES
+
+let example1 = false
+// Prints false
+console.log(Boolean(example1))
+
+let example2 = true
+// Prints false
+console.log(Boolean(example2))
+
+let example3 = null // Developer uses null to set a placeholder
+// Prints false
+console.log(Boolean(example3))
+
+let example4 = undefined
+// Prints false
+console.log(Boolean(example4))
+
+let example5 = ''
+// Prints false
+console.log(Boolean(example5))
+
+let example6 = NaN
+// Prints false
+console.log(Boolean(example6))
+
+let example7 = -5
+// Prints true
+console.log(Boolean(example7))
+
+let example8 = 0
+// Prints false
+console.log(Boolean(example8))
 
 // A function is a set of instructions
